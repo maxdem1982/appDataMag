f388697df42eefb051142a51dae4b089ebe3e47b
massimo <you@example.com>
2018-08-17 19:50:42 +0200

    login con validazione

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgControlStatusGroup, FormBuilder, FormGroup, Validators, FormControl } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  myForm: FormGroup;
+  userInfo: {name: string, email: string, phone: string, anni: number} = {name: '', email: '', phone: '', anni: 0};
 
+  constructor(public formBuilder: FormBuilder) {
+    
+  }
+
+  ngOnInit(): any {
+    this.myForm = this.formBuilder.group({
+      'name': ['', [Validators.required, Validators.minLength(3), this.nameValidator.bind(this)]],
+      'phone': ['', this.phoneValidator.bind(this)],
+      'email': ['', [Validators.required, this.emailValidator.bind(this)]],
+      'anni': ['', [Validators.required, this.anniValidator.bind(this)]]
+    });
+  }
+
+  onSubmit() {
+    console.log('submitting form');
+  }
+
+  isValid(field: string) {
+    let formField = this.myForm.get(field);
+    return formField.valid || formField.pristine;
+  }
+
+  nameValidator(control: FormControl): {[s: string]: boolean} {
+    if (!control.value.match("^[a-zA-Z ,.'-]+$")) {
+      return {invalidName: true};
+    }
+  }
+
+  phoneValidator(control: FormControl): {[s: string]: boolean} {
+    if (control.value !== '') {
+      if (!control.value.match('\\(?\\d{3}\\)?-? *\\d{3}-? *-?\\d{4}')) {
+        return {invalidPhone: true};
+      }
+    }
+  }
+
+  emailValidator(control: FormControl): {[s: string]: boolean} {
+    if (!(control.value.toLowerCase().match('^[a-zA-Z]\\w*@gmail\\.com$') || control.value.toLowerCase().match('^[a-zA-Z]\\w*@yahoo\\.com$'))) {
+      return {invalidEmail: true};
+    }
+  }
+
+  anniValidator(control: FormControl): {[s: string]: boolean} {
+    if ((control.value <= 14 || control.value > 120)) {
+      return {invalidAnni: true};
+    }
   }
 
 }
